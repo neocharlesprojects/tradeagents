691d6a4dd58a849e6e9fb4823b36cf61834d883f
Neo Charles <neocharlesprojects@gmail.com>
2025-01-25 00:04:13 +0000

    Add files via upload

---
+++ trade_agents/economics.py
@@ -0,0 +1,9 @@
+
+class Economics:
+    @staticmethod
+    def calculate_market_equilibrium(supply, demand):
+        return (supply + demand) / 2
+
+    @staticmethod
+    def simulate_market_shock():
+        return "Market shock simulated"
